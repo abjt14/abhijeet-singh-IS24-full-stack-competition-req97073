dc568193a8c98deccab029b10010b61b745446fb
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-03-28 17:52:00 -0700

    added check for id and dates while adding products

--- src/helpers/schema.ts
+++ src/helpers/schema.ts
@@ -1,28 +1,31 @@
 import { z } from "zod";
-import { productId, startDate } from "./regex";
+import { productIdRegExp, startDateRegExp } from "./regex";
 import { getCurrentDate } from "./utils";
 
 export const addProductSchema = z.object({
+  productId: z.string().regex(productIdRegExp),
   productName: z.string().min(1),
   productOwnerName: z.string().min(1),
   developers: z.array(z.string()).min(1),
   scrumMasterName: z.string().min(1),
-  startDate: z.string().regex(startDate).refine((date) => date <= getCurrentDate(), {
+  startDate: z.string().regex(startDateRegExp).refine((date) => date <= getCurrentDate(), {
     message: "startDate cannot be in the future.",
   }),
   methodology: z.enum(["Agile", "Waterfall"]),
 });
 
 export const updateProductSchema = z.object({
-  productId: z.string().regex(productId),
+  productId: z.string().regex(productIdRegExp),
   productName: z.optional(z.string().min(1)),
   productOwnerName: z.optional(z.string().min(1)),
   developers: z.optional(z.array(z.string()).min(1)),
   scrumMasterName: z.optional(z.string().min(1)),
-  startDate: z.optional(z.string().min(1)),
+  startDate: z.optional(z.string().regex(startDateRegExp).refine((date) => date <= getCurrentDate(), {
+    message: "startDate cannot be in the future.",
+  })),
   methodology: z.optional(z.enum(["Agile", "Waterfall"])),
 });
 
 export const deleteProductSchema = z.object({
-  productId: z.string().regex(productId),
+  productId: z.string().regex(productIdRegExp),
 });
