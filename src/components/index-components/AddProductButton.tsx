bda674ca4688d205a844ed8c6105a9ae62ea9c3b
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-03-29 17:58:14 -0700

    renamed project to product and cleaned up unudes code

--- src/components/index-components/AddProductButton.tsx
+++ src/components/index-components/AddProductButton.tsx
@@ -1,13 +1,11 @@
-import { Product } from "@/types/types";
 import { useState } from "react";
-import AddModel from "./Modals/AddModal";
+import AddModal from "./Modals/AddModal";
 
 interface AddProjectProps {
-  state: Product[];
   fetchHelper: Function;
 }
 
-export default function AddProjectButton({ state, fetchHelper }: AddProjectProps) {
+export default function AddProductButton({ fetchHelper }: AddProjectProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   function handleAddProduct() {
@@ -17,16 +15,15 @@ export default function AddProjectButton({ state, fetchHelper }: AddProjectProps
   return (
     <>
       <button
-        className="text-green-kelp-50 bg-green-kelp-800 hover:bg-green-kelp-700 duration-75 pl-2 pr-4 py-1 text-sm flex items-center justify-between gap-1 rounded-md"
+        className="h-fit text-green-kelp-50 bg-green-kelp-800 hover:bg-green-kelp-700 transition-all duration-75 pl-2 pr-4 py-2 text-sm flex items-center justify-between gap-1 rounded-md focus:outline-none focus:ring-2 focus:ring-green-kelp-700 focus:ring-opacity-50"
         onClick={() => handleAddProduct()}
       >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
         </svg>
-        <span className="pointer-events-none">Add Product</span>
+        <span className="pointer-events-none whitespace-nowrap">Add Product</span>
       </button>
-      <AddModel
-        type="add"
+      <AddModal
         fetchHelper={fetchHelper}
         isOpen={isOpen}
         setIsOpen={setIsOpen}
