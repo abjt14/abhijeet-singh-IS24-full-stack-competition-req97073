4deed896e1768b6b033747573ac5505b92d2d721
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-03-29 18:08:07 -0700

    added new error responses on existing name check for update and simple get product by id

--- src/helpers/responses.ts
+++ src/helpers/responses.ts
@@ -5,6 +5,17 @@ export const responses = {
       errorMessage: 'Could not retrieve the requested information. Please try again.',
     }
   },
+  getProduct: {
+    schema: {
+      successMessage: 'Product retrieved successfully.',
+      errorMessage: 'The product you are trying to retrieve does not exist.',
+    },
+    validator: {
+      successMessage: 'Product retrieved successfully.',
+      error: 'productId does not exist.',
+      errorMessage: 'The product you are trying to retrieve does not exist.',
+    }
+  },
   addProduct: {
     schema: {
       successMessage: 'Product added successfully.',
@@ -25,6 +36,8 @@ export const responses = {
       successMessage: 'Product updated successfully.',
       error: 'productId does not exist.',
       errorMessage: 'The product you are trying to update does not exist.',
+      error2: 'productName already exists.',
+      errorMessage2: 'The product name you are trying to use already exists.',
     }
   },
   deleteProduct: {
