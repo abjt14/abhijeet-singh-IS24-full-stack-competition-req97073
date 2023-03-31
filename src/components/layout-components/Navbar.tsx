d48fb45ef603fd2bc7d9b1fa63f5be6d88b61c34
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-03-31 04:22:04 -0700

    fixed route name for swagger api-docs

--- src/components/layout-components/Navbar.tsx
+++ src/components/layout-components/Navbar.tsx
@@ -28,7 +28,7 @@ export default function Navbar() {
           </svg>
           Ministry of Citizens
         </Link>
-        <Link href="/api/api-doc" className="text-lg flex gap-2 justify-center items-center px-2 py-1 rounded-md hover:bg-green-kelp-700 transition-all duration-75">
+        <Link href="/api/api-docs" className="text-lg flex gap-2 justify-center items-center px-2 py-1 rounded-md hover:bg-green-kelp-700 transition-all duration-75">
           API
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5">
             <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
